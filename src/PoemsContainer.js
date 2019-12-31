fb382ebc094e733c1f58bbdd504666562ad3c196
Jeffrey Paulino <jp7kode@gmail.com>
2019-12-31 09:47:49 -0500

    branching

--- src/PoemsContainer.js
+++ src/PoemsContainer.js
@@ -2,6 +2,15 @@ import React from 'react';
 import Poem from './Poem';
 
 class PoemsContainer extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/poems')
+        .then(data => data.json())
+        .then(console.log)
+        .catch(console.error)
+  }
+
+
   render(){
     return (
       <div className="poems-container">
